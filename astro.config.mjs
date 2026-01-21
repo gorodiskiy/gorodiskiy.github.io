5040b5ac8f317d66883e465b35add51594fb459f
gorodiskiy <gorodiskiy4@gmail.com>
2026-01-21 18:39:34 +0200

    deplou

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,11 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gorodiskiy.github.io',
+  i18n: {
+    defaultLocale: 'uk',
+    locales: ['uk', 'ru'],
+    routing: {
+      prefixDefaultLocale: false
+    }
+  }
 });
